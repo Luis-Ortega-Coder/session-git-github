68175bab93e3803308c6b831a544b26ddf3a3d12
Luis-Ortega-Coder <luisdaniloortegacalero17@gmail.com>
2025-03-26 11:39:48 -0500

    Este Es El Proyecto #3

---
+++ proyecto3.py
@@ -0,0 +1,30 @@
+
+menu = [" "," ============================================================","||----------------------------------------------------------||",
+        "||                           MENÚ                           ||",
+        "||----------------------------------------------------------||",
+        "||    1). Verificacion De Aprobado                          ||",
+        "||    2). Ingreso De Notas Y Calculo Del Promedio           ||",
+        "||    3). Contar Calificaciones Mayores                     ||",
+        "||    4). Verificar y Contar Calificaciones Epecificas      ||",
+        "||    5). Menu                                              ||",
+        "||    6). Salir Del Programa                                ||",
+        "||----------------------------------------------------------||",
+        " ============================================================"," "]
+for hola in menu:
+    print(hola)
+
+
+respuesta_menu = input()
+
+respuesta = input()
+
+while True:
+    print("Vas a Ingrezar Otro Producto: (S/N)")
+    nombre_del_producto = input("Igresa el Nombre Del Producto:")
+    PrecioDelProducto = input("Ingresa El Precio Del Producto: ")
+    CantidadDeProdustos = input("Ingresa La Cantidad De Productos Que Tiene Tu Stock: ")
+
+
+            
+    
+    
